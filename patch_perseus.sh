288fc8147b103997688a848675fcf7ed2f9b1c8f
Feathers <2767378157@qq.com>
2024-10-29 23:17:43 +0800

    change to JMBQ

--- patch_perseus.sh
+++ patch_perseus.sh
@@ -31,28 +31,27 @@ if [ ! -f "com.bilibili.AzurLane.apk" ]; then
     #if you want to patch .xapk, change the suffix here to wget "your download link" -O "your packge name.xapk" -q
     wget https://pkg.biligame.com/games/blhx_8.2.1_0820_1_20240830_041443_51682.apk -O com.bilibili.AzurLane.apk -q
     echo "apk downloaded !"
-    
+
     # if you can only download .xapk file uncomment 2 lines below. (delete the '#')
     #unzip -o com.YoStarJP.AzurLane.xapk -d AzurLane
     #cp AzurLane/com.YoStarJP.AzurLane.apk .
 fi
 
-# Download Perseus
-if [ ! -d "Perseus" ]; then
-    echo "Downloading Perseus"
-    git clone https://github.com/Egoistically/Perseus
+# Download JMBQ
+if [ ! -d "azurlane" ]; then
+    echo "download JMBQ"
+    git clone https://github.com/feathers-l/azurlane
 fi
 
 echo "Decompile Azur Lane apk"
 java -jar apktool.jar -q -f d com.bilibili.AzurLane.apk
 
-echo "Copy Perseus libs"
-cp -r Perseus/. com.bilibili.AzurLane/lib/
+echo "Copy JMBQ libs"
+cp -r azurlane/. com.bilibili.AzurLane/lib/
 
-echo "Patching Azur Lane with Perseus"
+echo "Patching Azur Lane with JMBQ"
 oncreate=$(grep -n -m 1 'onCreate' com.bilibili.AzurLane/smali_classes2/com/unity3d/player/UnityPlayerActivity.smali | sed  's/[0-9]*\:\(.*\)/\1/')
-sed -ir "s#\($oncreate\)#.method private static native init(Landroid/content/Context;)V\n.end method\n\n\1#" com.bilibili.AzurLane/smali_classes2/com/unity3d/player/UnityPlayerActivity.smali
-sed -ir "s#\($oncreate\)#\1\n    const-string v0, \"Perseus\"\n\n\    invoke-static {v0}, Ljava/lang/System;->loadLibrary(Ljava/lang/String;)V\n\n    invoke-static {p0}, Lcom/unity3d/player/UnityPlayerActivity;->init(Landroid/content/Context;)V\n#" com.bilibili.AzurLane/smali_classes2/com/unity3d/player/UnityPlayerActivity.smali
+sed -ir "N; s#\($oncreate\n    .locals 2\)#\1\n    const-string v0, \"JMBQ\"\n\n    invoke-static {v0}, Ljava/lang/System;->loadLibrary(Ljava/lang/String;)V\n#" com.bilibili.AzurLane/smali_classes2/com/unity3d/player/UnityPlayerActivity.smali
 
 echo "Build Patched Azur Lane apk"
 java -jar apktool.jar -q -f b com.bilibili.AzurLane -o build/com.bilibili.AzurLane.patched.apk
